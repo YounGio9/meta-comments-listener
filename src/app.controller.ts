d8702274a51b7fc6eff11df2d18873538b08f841
Giovani de SOUZA <desouzagiovani9@gmail.com>
2024-10-30 16:57:27 +0100

    feat: Add logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,17 +8,18 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { GetWebHookQueryDto } from './dto';
+import { logger } from './config/logger.config';
 
 @Controller('webhook')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('whatsapp')
+  @Get('instagram')
   handleGetWebhook(@Query() query: GetWebHookQueryDto): string {
     const mode = query['hub.mode'];
     const challenge = query['hub.challenge'];
     const token = query['hub.verify_token'];
-
+    logger.info(query);
     if (mode && token) {
       if (mode === 'subscribe' && token === 'verifyTokenSecret') {
         return challenge;
@@ -30,8 +31,9 @@ export class AppController {
     }
   }
 
-  @Post('whatsapp')
+  @Post('instagram')
   handlePostWebhook(@Body() payload: any) {
-    return payload;
+    logger.info(payload);
+    return '';
   }
 }
