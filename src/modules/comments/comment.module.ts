5dac5ed128e95882ac38eb2f4899b38c226370e3
Giovani de SOUZA <desouzagiovani9@gmail.com>
2024-11-06 16:07:34 +0100

    feat: Update dependancies

--- src/modules/comments/comment.module.ts
+++ src/modules/comments/comment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { InstagramService } from '../instagram/instagram.service';
+import { FacebookService } from '../facebook/facebook.service';
 
 @Module({
   controllers: [CommentController],
-  providers: [InstagramService],
+  providers: [InstagramService, FacebookService],
 })
 export class CommentModule {}
